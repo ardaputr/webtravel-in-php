8e722d6f85bddbdc60f3b72e9152c2994e98cd9a
412d4a <123223445+ardaputr@users.noreply.github.com>
2023-11-16 17:55:23 +0700

    by arda

--- home.php
+++ home.php
@@ -19,31 +19,50 @@ if (!isset($_SESSION['loggedin']) && $_SESSION['loggedin'] !== false) {
 
 <body>
 	<header class="header" id="header">
-		<ul class="nav nav-underline">
-			<li class="nav-item">
-				<a class="nav-link active" aria-current="page" href="home.php">Home</a>
-			</li>
-			<li class="nav-item">
-				<a class="nav-link" href="#">Link</a>
-			</li>
-			<li class="nav-item">
-				<a class="nav-link" href="#">Link</a>
-			</li>
-			<li class="nav-item">
-				<a class="nav-link disabled" aria-disabled="true">Disabled</a>
-			</li>
-		</ul>
+		<nav class="navbar navbar-dark bg-dark fixed-top">
+			<div class="container-fluid">
+				<a class="navbar-brand" href="#">Boole</a>
+				<button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
+					<div class="offcanvas-header">
+						<h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">Hai, <?php echo $_SESSION['username']; ?></h5>
+						<button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+					</div>
+					<div class="offcanvas-body">
+						<ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+							<li class="nav-item">
+								<a class="nav-link active" aria-current="page" href="home.php">Home</a>
+							</li>
+							<li class="nav-item dropdown">
+								<a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+									Profile
+								</a>
+								<ul class="dropdown-menu dropdown-menu-dark">
+
+									<li><a class="dropdown-item" href="password_reset.php">Reset Password</a></li>
+									<li><a class="dropdown-item" href="logout.php">Sign Out</a></li>
+									<li>
+
+										<hr class="dropdown-divider">
+									</li>
+									<li><a class="dropdown-item" href="#">Something else here</a></li>
+								</ul>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</nav>
 	</header>
 
 	<main>
 		<section class="container wrapper">
 			<div class="page-header">
-				<h2 class="display-5">Welcome : <?php echo $_SESSION['username']; ?></h2>
+				<br>
+				<h2 class="display-5">Welcome</h2>
 			</div>
-			<center>
-				<a href="password_reset.php" class="btn btn-block btn-outline-warning">Reset Password</a>
-				<a href="logout.php" class="btn btn-block btn-outline-danger">Sign Out</a>
-			</center>
 		</section>
 	</main>
 
