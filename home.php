a64a7e6eb8631acf66e0e24f79d7ff365fcc355c
yeilahlan <nolantabiin15@gmail.com>
2023-11-17 23:05:14 +0700

    Home Admin

--- home.php
+++ home.php
@@ -14,64 +14,102 @@ if (!isset($_SESSION['loggedin']) && $_SESSION['loggedin'] !== false) {
 	<meta charset="UTF-8">
 	<title>Welcome</title>
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-	<link rel="stylesheet" href="css/login.css">
+	<link rel="stylesheet" href="css/home.css">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;800&display=swap" rel="stylesheet">
 </head>
 
 <body>
 	<header class="header" id="header">
-		<nav class="navbar navbar-dark bg-dark fixed-top">
-			<div class="container-fluid">
-				<a class="navbar-brand" href="#">Boole</a>
-				<button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
-					<span class="navbar-toggler-icon"></span>
+		<section style="background-image: url('bg1.jpg'); background-size: cover; height:550px; border-bottom-left-radius: 20px; border-bottom-right-radius: 20px;">
+		<nav>
+		<div>
+			<ul class="nav justify-content-center navbar">
+			<a class="navbar-brand me-auto" href="#" style="font-weight:600; color: #ffffff; font-size:30px;">Boole</a>
+				<li class="nav-item">
+					<a class="nav-link" href="#" style="color: #FFFFF;" id="hover">Home</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#" style="color: #FFFFFF;" id="hover">Add</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#" style="color: #FFFFFF;" id="hover">Edit</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#" style="color: #FFFFFF;" id="hover">Delete</a>
+				</li>
+				<button class="navbar-toggler ms-auto" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasLightNavbar" aria-controls="offcanvasLightNavbar" aria-label="Toggle navigation">
+					<span>
+						<iconify-icon icon="iconamoon:profile-circle-fill" style="color: white;" width="40" height="40"></iconify-icon>
+					</span>
 				</button>
-				<div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
+				<div class="offcanvas offcanvas-end text-bg-light" tabindex="-1" id="offcanvasLightNavbar" aria-labelledby="offcanvasLightNavbarLabel">
 					<div class="offcanvas-header">
-						<h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">Hai, <?php echo $_SESSION['username']; ?></h5>
-						<button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+						<h5 class="offcanvas-title" id="offcanvasLightNavbarLabel">Hai, <?php echo $_SESSION['username']; ?></h5>
+						<button type="button" class="btn-close btn-close-black" data-bs-dismiss="offcanvas" aria-label="Close"></button>
 					</div>
 					<div class="offcanvas-body">
 						<ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
 							<li class="nav-item">
 								<a class="nav-link active" aria-current="page" href="home.php">Home</a>
 							</li>
-							<li class="nav-item dropdown">
-								<a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-									Profile
-								</a>
-								<ul class="dropdown-menu dropdown-menu-dark">
-
-									<li><a class="dropdown-item" href="password_reset.php">Reset Password</a></li>
-									<li><a class="dropdown-item" href="logout.php">Sign Out</a></li>
-									<li>
-
-										<hr class="dropdown-divider">
-									</li>
-									<li><a class="dropdown-item" href="#">Something else here</a></li>
-								</ul>
+							<li>
+								<a class="nav-link" href="password_reset.php">Reset Password</a>
+							</li>
+							<li>
+								<a class="nav-link" href="logout.php">Sign Out</a>
 							</li>
 						</ul>
 					</div>
 				</div>
-			</div>
+		</div>
 		</nav>
+		<div class="intro">
+			<h1 style="font-weight:800;">Your Doorway <br>To Endless Travel Wonders</h1>
+			<p>Ready to redefine your travel experience? Boole is your guide <br>to extraordinary destinations. Click, discover, and wander!</p>
+		</div>
+		</section>
+		<section class="explore">
+			<h1 style="font-weight:600;">Explore to Destination</h1>
+			<div class="container text-center" style="padding-top:20px;">
+				<div class="row align-items-start">
+					<div class="col">
+						<div class="card" style="width: 18rem;">
+						<img src="..." class="card-img-top" alt="...">
+							<div class="card-body">
+								<h5 class="card-title">Card title</h5>
+								<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+								<a href="#" class="btn btn-primary">Go somewhere</a>
+							</div>
+						</div>
+					</div>
+					<div class="col">
+						<div class="card" style="width: 18rem;">
+						<img src="..." class="card-img-top" alt="...">
+							<div class="card-body">
+								<h5 class="card-title">Card title</h5>
+								<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+								<a href="#" class="btn btn-primary">Go somewhere</a>
+							</div>
+						</div>
+					</div>
+					<div class="col">
+						<div class="card" style="width: 18rem;">
+						<img src="..." class="card-img-top" alt="...">
+							<div class="card-body">
+								<h5 class="card-title">Card title</h5>
+								<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+								<a href="#" class="btn btn-primary">Go somewhere</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
 	</header>
-
-	<main>
-	<section class="film">
-    <div class="film-container">
-      <div class="column-left">
-        <h1>Welcome to <span>Boole</span> </h1>
-        <p>Your ....</p>
-          <a href="quiz.html"><button type="button">Start Travelling</button></a>
-      </div>
-      <div class="column-right">
-        <img src="logoalala.jpg" class="film-image">
-      </div>
-    </div>
-	</main>
-
+	<script src="https://code.iconify.design/iconify-icon/1.0.7/iconify-icon.min.js"></script>
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
-
 </html>
+
