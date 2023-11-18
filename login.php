abec7b2cc512143aa08f3738c9365d2583ffa89b
412d4a <123223445+ardaputr@users.noreply.github.com>
2023-11-18 16:49:48 +0700

    update login

--- login.php
+++ login.php
@@ -99,8 +99,20 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
       text-align: center;
     }
 
+    label {
+      margin-right: 5px;
+    }
+
     .form-group {
       padding: 5px;
+    }
+
+    .show-password {
+      cursor: pointer;
+      position: absolute;
+      right: 20px;
+      top: 59%;
+      transform: translateY(-50%);
     }
   </style>
 
@@ -111,17 +123,18 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <section class="wrapper">
       <h2>Login</h2>
       <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST">
-        <div class="form-group <?php (!empty($username_err)) ? 'has_error' : ''; ?>">
+        <div class="form-group <?php echo (!empty($username_err)) ? 'has_error' : ''; ?>">
           <label for="username">Username</label>
           <i class='bx bxs-user'></i>
           <input type="text" name="username" id="username" class="form-control" placeholder="Input Username">
           <span class="help-block"><?php echo $username_err; ?></span>
         </div>
 
-        <div class="form-group <?php (!empty($password_err)) ? 'has_error' : ''; ?>">
+        <div class="form-group <?php echo (!empty($password_err)) ? 'has_error' : ''; ?>">
           <label for="password">Password</label>
           <i class='bx bxs-key'></i>
           <input type="password" name="password" id="password" class="form-control" placeholder="Input Password">
+          <i class="bx bx-hide show-password" onclick="togglePasswordVisibility('password')"></i>
           <span class="help-block"><?php echo $password_err; ?></span>
         </div>
 
@@ -130,9 +143,25 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         </div>
         <p>Don't have an account? <a href="register.php">Sign in</a>.</p>
       </form>
-
     </section>
   </main>
+
+  <script>
+    function togglePasswordVisibility(inputId) {
+      var passwordInput = document.getElementById(inputId);
+      var icon = document.querySelector('.show-password');
+
+      if (passwordInput.type === 'password') {
+        passwordInput.type = 'text';
+        icon.classList.remove('bx-hide');
+        icon.classList.add('bx-show');
+      } else {
+        passwordInput.type = 'password';
+        icon.classList.remove('bx-show');
+        icon.classList.add('bx-hide');
+      }
+    }
+  </script>
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
