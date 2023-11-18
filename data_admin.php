7029732ebe8a8a60fe4d7ead8a9e55211f3b749c
412d4a <123223445+ardaputr@users.noreply.github.com>
2023-11-18 18:32:28 +0700

    update

--- data_admin.php
+++ data_admin.php
@@ -1,25 +1,15 @@
-<?php
-session_start();
-
-if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] === false) {
-    header('location: login.php');
-    exit;
-}
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin Dashboard</title>
+    <title>Data Admin</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
 </head>
 
 <body>
-
     <nav class="navbar navbar-dark bg-dark fixed-top">
         <div class="container-fluid">
             <a class="navbar-brand">Admin Dashboard</a>
@@ -68,6 +58,44 @@ if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] === false) {
             </div>
         </div>
     </nav>
+
+    <br><br><br>
+    <section class="container">
+        <title>User Data</title>
+        <?php
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "boole_db";
+
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+
+        $sql = "SELECT username, password, role, created_at FROM users WHERE role='admin'";
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) {
+            echo '<table border="1">';
+            echo '<tr><th>Username</th><th>Password</th><th>Role</th><th>Created At</th></tr>';
+            while ($row = $result->fetch_assoc()) {
+                echo '<tr>';
+                echo '<td>' . $row['username'] . '</td>';
+                echo '<td>' . $row['password'] . '</td>';
+                echo '<td>' . $row['role'] . '</td>';
+
+
+                echo '<td>' . $row['created_at'] . '</td>';
+                echo '</tr>';
+            }
+            echo '</table>';
+        } else {
+            echo "No records found";
+        }
+        $conn->close();
+        ?>
+    </section>
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
