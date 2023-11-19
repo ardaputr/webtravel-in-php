22642a332e6ba68a02ece8ba5182af42ec98a652
412d4a <123223445+ardaputr@users.noreply.github.com>
2023-11-19 17:25:52 +0700

    update

--- admin_home.php
+++ admin_home.php
@@ -1,72 +1,31 @@
-<?php
-session_start();
-
-if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] === false) {
-    header('location: login.php');
-    exit;
-}
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin Dashboard</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Navbar di Pinggir Kiri</title>
+  <link rel="stylesheet" href="css/dashboard.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;800&display=swap" rel="stylesheet">
 </head>
 
 <body>
 
-    <nav class="navbar navbar-dark bg-dark fixed-top">
-        <div class="container-fluid">
-            <a class="navbar-brand">Admin Dashboard</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
-                <div class="offcanvas-header">
-                    <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">Menu</h5>
-                    <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-                </div>
-                <div class="offcanvas-body">
-                    <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="admin_home.php">Home</a>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Data User
-                            </a>
-                            <ul class="dropdown-menu dropdown-menu-dark">
-                                <li><a class="dropdown-item" href="data_admin.php">Data Admin</a></li>
-                                <li><a class="dropdown-item" href="data_user.php">Data User</a></li>
-                            </ul>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Data Destination
-                            </a>
-                            <ul class="dropdown-menu dropdown-menu-dark">
-                                <li><a class="dropdown-item" href="list_destination.php">List Destination</a></li>
-                                <li><a class="dropdown-item" href="add_destination.php">Add Destination</a></li>
-                            </ul>
-                        </li>
-                        <center>
-                            <li class="nav-item">
-                                <a class="nav-link" href="logout.php">Sign Out</a>
-                            </li>
-                        </center>
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </nav>
+  <div class="navbar">
+    <a href="data_admin.php">Admin Data</a>
+    <a href="data_user.php">User Data</a>
+    <a href="#">Destination List</a>
+    <a href="#">Add Destination</a>
+    <a href="logout.php">Sign Out</a>
+  </div>
 
+  <div class="content">
+    <h2>Konten Utama</h2>
+    <p>Ini adalah halaman utama dengan navbar di sisi kiri.</p>
+  </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 
 </html>
--- data_user.php
+++ data_user.php
@@ -26,127 +26,48 @@ $result = $conn->query($sql);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Data User</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            background-color: #f4f4f4;
-            margin: 0;
-            padding: 0;
-            box-sizing: border-box;
-        }
-
-        .container {
-            max-width: 800px;
-            margin: 20px auto;
-            padding: 20px;
-            background-color: #fff;
-            border-radius: 8px;
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-        }
-
-        table {
-            width: 100%;
-            border-collapse: collapse;
-            margin-top: 20px;
-        }
-
-        th,
-        td {
-            padding: 12px;
-            text-align: left;
-            border-bottom: 1px solid #ddd;
-        }
-
-        th {
-            background-color: darkblue;
-            color: #fff;
-        }
-
-        tr:hover {
-            background-color: #f5f5f5;
-        }
-
-        td {
-            word-break: break-all;
-        }
-    </style>
+    <title>Navbar di Pinggir Kiri</title>
+    <link rel="stylesheet" href="css/dashboard.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;800&display=swap" rel="stylesheet">
 </head>
 
 <body>
-    <nav class="navbar navbar-dark bg-dark fixed-top">
-        <div class="container-fluid">
-            <a class="navbar-brand">Admin Dashboard</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
-                <div class="offcanvas-header">
-                    <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">Menu</h5>
-                    <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-                </div>
-                <div class="offcanvas-body">
-                    <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="admin_home.php">Home</a>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Account Data
-                            </a>
-                            <ul class="dropdown-menu dropdown-menu-dark">
-                                <li><a class="dropdown-item" href="data_admin.php">Admin Data</a></li>
-                                <li><a class="dropdown-item" href="data_user.php">User Data</a></li>
-                            </ul>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Destination Data
-                            </a>
-                            <ul class="dropdown-menu dropdown-menu-dark">
-                                <li><a class="dropdown-item" href="list_destination.php">List Destination</a></li>
-                                <li><a class="dropdown-item" href="add_destination.php">Add Destination</a></li>
-                            </ul>
-                        </li>
-                        <center>
-                            <li class="nav-item">
-                                <a class="nav-link" href="logout.php">Sign Out</a>
-                            </li>
-                        </center>
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </nav>
 
-    <br><br><br>
-    <section class="container">
-        <title>User Data</title>
-        <?php
-        if ($result->num_rows > 0) {
-            echo '<table border="1">';
-            echo '<tr><th>Username</th><th>Password</th><th>User Type</th><th>Sign Up Date</th><th>Action</th></tr>';
-            while ($row = $result->fetch_assoc()) {
-                echo '<tr>';
-                echo '<td>' . $row['username'] . '</td>';
-                echo '<td>' . $row['password'] . '</td>';
-                echo '<td>' . $row['role'] . '</td>';
-                echo '<td>' . $row['created_at'] . '</td>';
-                echo '<td><form method="post"><input type="hidden" name="id" value="' . $row['id'] . '"><button type="submit" class="btn btn-danger">Delete</button></form></td>';
-                echo '</tr>';
+    <div class="navbar">
+        <a href="data_admin.php">Admin Data</a>
+        <a href="data_user.php" class="active">User Data</a>
+        <a href="#">Destination List</a>
+        <a href="#">Add Destination</a>
+        <a href="logout.php">Sign Out</a>
+    </div>
+
+    <div class="content">
+        <section class="container">
+            <title>User Data</title>
+            <?php
+            if ($result->num_rows > 0) {
+                echo '<table border="1">';
+                echo '<tr><th>Username</th><th>Password</th><th>User Type</th><th>Sign Up Date</th><th>Action</th></tr>';
+                while ($row = $result->fetch_assoc()) {
+                    echo '<tr>';
+                    echo '<td>' . $row['username'] . '</td>';
+                    echo '<td>' . $row['password'] . '</td>';
+                    echo '<td>' . $row['role'] . '</td>';
+                    echo '<td>' . $row['created_at'] . '</td>';
+                    echo '<td><form method="post"><input type="hidden" name="id" value="' . $row['id'] . '"><button type="submit" class="btn btn-danger">Delete</button></form></td>';
+                    echo '</tr>';
+                }
+                echo '</table>';
+            } else {
+                echo "No records found";
             }
-            echo '</table>';
-        } else {
-            echo "No records found";
-        }
-        $conn->close();
-        ?>
-    </section>
+            $conn->close();
+            ?>
+        </section>
+    </div>
 
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 
 </html>
