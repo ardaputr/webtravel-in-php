3f09d616d1de72404d8a7bff52ef26dfde28fc9d
412d4a <123223445+ardaputr@users.noreply.github.com>
2023-11-29 21:50:45 +0700

    up

--- detail.php
+++ detail.php
@@ -25,7 +25,6 @@ if (!$destination) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Destination Details</title>
-    <link rel="stylesheet" href="css/home.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 
@@ -42,9 +41,6 @@ if (!$destination) {
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="explore.php" style="color: #FFFFFF;" id="hover">Explore</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#" style="color: #FFFFFF;" id="hover">Blog</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="#" style="color: #FFFFFF;" id="hover">About Us</a>
